55dfe69fef9d79cf0b2f821b6f6012274600678a
Renat Idrisov <parsifal.47@gmail.com>
2014-09-27 19:07:13 +0700

    more complex sql :)

--- src-clj/acha/db.clj
+++ src-clj/acha/db.clj
@@ -32,19 +32,28 @@
   (insert! db :repo {:url "git@github.com:tonsky/datascript"})
   (insert! db :repo {:url "git@github.com:tonsky/41-socks"})
   (insert! db :repo {:url "git@github.com:tonsky/datascript-chat"})
-  (insert! db :repo {:url "git@github.com:tonsky/net.async"}))
+  (insert! db :repo {:url "git@github.com:tonsky/net.async"})
+  (insert! db :user {:name "Anders Hovmöller" :email "boxed@killingar.net"})
+  (insert! db :user {:name "Bobby Calderwood" :email "bobby_calderwood@mac.com"})
+  (insert! db :user {:name "Kevin J. Lynagh"  :email "kevin@keminglabs.com"})
+  (insert! db :user {:name "Nikita Prokopov"  :email "prokopov@gmail.com"})
+  (insert! db :user {:name "montyxcantsin"    :email "montyxcantsin@gmail.com"})
+  (insert! db :user {:name "thegeez"          :email "thegeez@users.noreply.github.com"})
+  )
 
 (defn get-repo-list []
-  (query db "select * from repo"))
+  (query db "SELECT r.*, count(a.id) AS achievements FROM repo r
+    LEFT JOIN achievement a ON r.id=a.repoid GROUP BY r.id"))
 
 (defn get-user-list []
-  (query db "select * from user"))
+  (query db "SELECT u.*, count(a.id) AS achievements FROM user u 
+    LEFT JOIN achievement a ON u.id=a.userid GROUP BY u.id"))
 
 (defn get-ach-list []
-  (query db "select * from achievement"))
+  (query db "SELECT * FROM achievement"))
 
 (defn get-user-ach [id]
-  (query db (str "select * from achievement where userid=" id)))
+  (query db (str "SELECT * FROM achievement WHERE userid=" id)))
 
 (defn get-repo-ach [id]
-  (query db (str "select * from achievement where repoid=" id)))
+  (query db (str "SELECT * FROM achievement WHERE repoid=" id)))
