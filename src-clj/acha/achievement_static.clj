5a30556d2efacc64c2b0d8b245b38c7aa9602bbf
Dmitry Ivanov <ethercrow@gmail.com>
2014-09-27 17:06:41 +0700

    Separate achievement scanners and static data.

---
+++ src-clj/acha/achievement_static.clj
@@ -0,0 +1,12 @@
+(ns acha.achievement-static)
+
+(def table
+  {:christmas
+   {:name "Ruined Christmas"
+    :description "Commit something on 25th of December."
+    :picture "..."}
+   :halloween
+   {:name "This code looks scary"
+    :description "Commit something on 31th of October"
+    :picture "..."}
+   })
