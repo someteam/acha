442af385bbd9487b1fad188fedb76d65a30ebda2
Dmitry Ivanov <ethercrow@gmail.com>
2014-09-27 21:41:31 +0700

    Test for language scanner.

--- test/acha/achievement_test.clj
+++ test/acha/achievement_test.clj
@@ -6,7 +6,17 @@
   (let [time #inst "2014-04-20T17:29:59.000-00:00"]
     (is (= {:time time
             :username "Bender"}
-          ((last (achievement/make-date-scanner :foo 3 20))
+          ((last (achievement/make-date-scanner [:foo 3 20]))
            {:time time
             :author "Bender"
             })))))
+
+(deftest language-scanner
+  (let [time #inst "2014-04-20T17:29:59.000-00:00"
+        diffs [[:added]]]
+    (is (= {:time time
+            :username "Bender"}
+          ((last (achievement/make-language-scanner [:clojure ["clj"]]))
+           {:time time
+            :author "Bender"
+            :changed-files [[:add {:id 123} {:id 345 :path "project.clj"}]]})))))
