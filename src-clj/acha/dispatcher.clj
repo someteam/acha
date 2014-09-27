783fc0ecbe1fb47522b66e42e9d0ee4a9a4b0441
Andrey Vasenin <vaseninlol@gmail.com>
2014-09-27 16:43:17 +0700

    Integrate achievement with dispatcher

--- src-clj/acha/dispatcher.clj
+++ src-clj/acha/dispatcher.clj
@@ -1,17 +1,35 @@
 (ns acha.dispatcher
   (:require
     [clojure.tools.logging :as logging]
+    [acha.achievement :as achievement]
     [acha.git-parser :as git-parser]))
+
+(defn- apply-achievement [scanner commit-info]
+  (try
+    (when-let [result (scanner commit-info)]
+
+      ;; store it to database
+     ;; (logging/info result)
+      
+      )
+
+    (catch Exception e
+      (logging/error e "Error occured during achivement scan commit"))))
+
+(defn- analyze-commit [repo commit df]
+  (try
+    (let [commit-info (git-parser/commit-info repo commit df)]
+      (doseq [scanner achievement/all-commit-info-scanners]
+        (apply-achievement scanner commit-info)))
+    (catch Exception e
+      (logging/error e "Error occured during commit-info parsing" (.getName commit)))
+    (finally )))
+
 
 (defn analyze [url]
   (let [repo (git-parser/load-repo url)
         df (git-parser/diff-formatter repo)]
     (doseq [commit (git-parser/commit-list repo)]
-      (try
-        (let [commit-info (git-parser/commit-info repo commit df)]
-          (logging/info commit-info))
-        (catch Exception e
-          (logging/error e "Unable to parse commit"))
-        (finally 
-          ;; mark this tweets as viewed
-          )))))
+      (analyze-commit repo commit df))))
+
+
