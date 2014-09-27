77fd8e9537cb78b1ec58224a0d77412aa6f17295
Dmitry Ivanov <ethercrow@gmail.com>
2014-09-27 18:07:17 +0700

    Stubs for all achievements.

--- src-clj/acha/achievement.clj
+++ src-clj/acha/achievement.clj
@@ -1,5 +1,6 @@
 (ns acha.achievement
   (:require
+    [acha.util :as util]
     [clojure.string :as str])
   (:import [java.util Calendar]))
 
@@ -39,47 +40,291 @@
    (fn [commit-info]
      nil)])
 
+(def bad-motherfucker
+  [:bad-motherfucker
+   (fn [{:keys [message author time]}]
+     (let [fuck-count (util/count-substring-occurrences message "fuck")]
+       (when (pos? fuck-count)
+         [{:level fuck-count
+           :username author
+           :time time}])))])
+
+(def swear-words
+  ["fuck" "shit" "damn" "sucks"])
+
+(def hello-linus
+  [:hello-linus
+   (fn [{:keys [message author time]}]
+     (let [count-word (fn [word] (util/count-substring-occurrences message word))
+           swear-count (reduce + (map count-word swear-words))]
+       (when (pos? swear-count)
+         [{:level swear-count
+           :username author
+           :time time}])))])
+
 ; TODO commit-info achievements
-(defn bad-motherfucker [commit-info] nil)
-(defn hello-linus [commit-info] nil)
-(defn man-of-few-words [commit-info] nil)
-(defn leo-tolstoy [commit-info] nil)
-(defn citation-needed [commit-info] nil)
-(defn no-more-letters [commit-info] nil)
-(defn cool-kid [commit-info] nil)
-(defn beggar [commit-info] nil)
-(defn borat [commit-info] nil)
-(defn never-probably [commit-info] nil)
-(defn professional-pride [commit-info] nil)
-(defn turkey-day [commit-info] nil)
+(def borat
+  [:borat
+   (fn [commit-info]
+     nil)])
+(def catchphrase
+  [:catchphrase
+   (fn [commit-info]
+     nil)])
+(def citation-needed
+  [:citation-needed
+   (fn [commit-info]
+     nil)])
+(def cool-kid
+  [:cool-kid
+   (fn [commit-info]
+     nil)])
+(def eraser
+  [:eraser
+   (fn [commit-info]
+     nil)])
+(def leo-tolstoy
+  [:leo-tolstoy
+   (fn [commit-info]
+     nil)])
+(def man-of-few-words
+  [:man-of-few-words
+   (fn [commit-info]
+     nil)])
+(def massive
+  [:massive
+   (fn [commit-info]
+     nil)])
+(def never-probably
+  [:never-probably
+   (fn [commit-info]
+     nil)])
+(def no-more-letters
+  [:no-more-letters
+   (fn [commit-info]
+     nil)])
+(def professional-pride
+  [:professional-pride
+   (fn [commit-info]
+     nil)])
+(def turkey-day
+  [:turkey-day
+   (fn [commit-info]
+     nil)])
+(def scribbler
+  [:scribbler
+   (fn [commit-info]
+     nil)])
+(def owl
+  [:owl
+   (fn [commit-info]
+     nil)])
+(def easy-fix
+  [:easy-fix
+   (fn [commit-info]
+     nil)])
+(def multilingual
+  [:multilingual
+   (fn [commit-info]
+     nil)])
+(def mover
+  [:mover
+   (fn [commit-info]
+     nil)])
+(def world-balance
+  [:world-balance
+   (fn [commit-info]
+     nil)])
+(def narcissist
+  [:narcissist
+   (fn [commit-info]
+     nil)])
+(def blamer
+  [:blamer
+   (fn [commit-info]
+     nil)])
+(def lucky
+  [:lucky
+   (fn [commit-info]
+     nil)])
+(def mark-of-the-beast
+  [:mark-of-the-beast
+   (fn [commit-info]
+     nil)])
+(def commenter
+  [:commenter
+   (fn [commit-info]
+     nil)])
+(def ocd
+  [:ocd
+   (fn [commit-info]
+     nil)])
+(def holy-war
+  [:holy-war
+   (fn [commit-info]
+     nil)])
+(def fat-ass
+  [:fat-ass
+   (fn [commit-info]
+     nil)])
+(def deal-with-it
+  [:deal-with-it
+   (fn [commit-info]
+     nil)])
+(def dangerous-game
+  [:dangerous-game
+   (fn [commit-info]
+     nil)])
+(def empty-commit
+  [:empty-commit
+   (fn [commit-info]
+     nil)])
+(def time-get
+  [:time-get
+   (fn [commit-info]
+     nil)])
+(def for-stallman
+  [:for-stallman
+   (fn [commit-info]
+     nil)])
+(def change-of-mind
+  [:change-of-mind
+   (fn [commit-info]
+     nil)])
+(def wrecking-ball
+  [:wrecking-ball
+   (fn [commit-info]
+     nil)])
+(def alzheimers
+  [:alzheimers
+   (fn [commit-info]
+     nil)])
+(def nothing-to-hide
+  [:nothing-to-hide
+   (fn [commit-info]
+     nil)])
 
 ; TODO timeline achievements
-(defn catchphrase [timeline] nil)
-(defn anniversary [timeline] nil)
+(def catchphrase
+  [:catchphrase
+   (fn [timeline]
+     nil)])
+(def anniversary
+  [:anniversary
+   (fn [timeline]
+     nil)])
+(def flash
+  [:flash
+   (fn [timeline]
+     nil)])
+(def waste
+  [:waste
+   (fn [timeline]
+     nil)])
+(def loneliness
+  [:loneliness
+   (fn [timeline]
+     nil)])
+(def necromancer
+  [:necromancer
+   (fn [timeline]
+     nil)])
+(def commit-get
+  [:commit-get
+   (fn [timeline]
+     nil)])
+(def collision
+  [:collision
+   (fn [timeline]
+     nil)])
+(def hydra
+  [:hydra
+   (fn [timeline]
+     nil)])
+(def peacemaker
+  [:peacemaker
+   (fn [timeline]
+     nil)])
+(def combo
+  [:combo
+   (fn [timeline]
+     nil)])
+(def combo-breaker
+  [:combo-breaker
+   (fn [timeline]
+     nil)])
+(def worker-bee
+  [:worker-bee
+   (fn [timeline]
+     nil)])
+(def oops
+  [:oops
+   (fn [timeline]
+     nil)])
+(def what-happened-here
+  [:what-happened-here
+   (fn [timeline]
+     nil)])
+(def all-things-die
+  [:all-things-die
+   (fn [timeline]
+     nil)])
+(def goodboy
+  [:goodboy
+   (fn [timeline]
+     nil)])
+
+; TODO meta achievements
+(def gandalf nil)
+(def munchkin nil)
+(def unpretending nil)
 
 (def date-table
   [[:christmas 11 25]
    [:halloween 9 31]
-   [:new-year 0 1]
-   [:valentine 1 14]
    [:leap-day 1 29]
-   [:russia-day 5 12]])
+   [:new-year 0 1]
+   [:russia-day 5 12]
+   [:valentine 1 14]])
 
 (def substring-table
-  [[:impossible "impossible"]
-   [:magic "magic"]
-   [:sorry "sorry"]
-   [:google "google"]
+  [[:fix "fix"]
    [:forgot "forgot"]
-   [:fix "fix"]
-   [:secure "secure"]
+   [:google "google"]
    [:hack "hack"]
+   [:impossible "impossible"]
+   [:magic "magic"]
+   [:secure "secure"]
+   [:sorry "sorry"]
+   [:beggar "achievement"]
    [:wow "wow"]])
 
 (def language-table
-  [[:haskell ["hs" "lhs"]]
+  [[:basic ["bas" "vb" "vbs" "vba"]]
+   [:c ["c"]]
+   [:c-sharp ["cs"]]
+   [:clojure ["clj" "cljx"]]
+   [:clojurescript ["cljs"]]
+   [:css ["css" "sass" "scss" "less" "haml"]]
+   [:cxx ["c++" "cc" "cpp" "cxx" "pcc" "hh" "hpp" "hxx"]]
+   [:dart ["dart"]]
+   [:duct-tape ["bash" "sh" "awk" "sed"]]
+   [:erlang ["erl" "hrl"]]
+   [:go ["go"]]
+   [:haskell ["hs" "lhs"]]
+   [:java ["java" "jsf" "jsp" "jspf"]]
+   [:javascript ["js"]]
+   [:objective-c ["m" "mm"]]
+   [:pascal ["pas"]]
    [:perl ["pl"]]
-   ])
+   [:php ["php" "php3" "php4" "php5"]]
+   [:python ["py"]]
+   [:ruby ["rake" "rb"]]
+   [:scala ["scala"]]
+   [:sql ["sql"]]
+   [:swift ["swift"]]
+   [:windows-language ["bat" "btm" "cmd" "ps1" "csproj" "vbproj" "vcproj" "wdproj" "wixproj" "xaml"]]
+   [:xml ["xml" "xsl" "xslt" "xsd" "dtd"]]])
 
 ; Scanner is 2-tuple of name and scanning function
 (def all-commit-info-scanners
@@ -87,23 +332,66 @@
     (map make-language-scanner language-table)
     (map make-date-scanner date-table)
     (map make-substring-scanner substring-table)
-    [professional-pride
-     turkey-day
-     catchphrase
-     bad-motherfucker
-     hello-linus
-     man-of-few-words
-     leo-tolstoy
-     citation-needed
-     no-more-letters
-     cool-kid
-     beggar
+    [bad-motherfucker
      borat
+     catchphrase
+     citation-needed
+     cool-kid
+     eraser
+     hello-linus
+     leo-tolstoy
+     man-of-few-words
+     massive
      never-probably
+     no-more-letters
+     professional-pride
+     turkey-day
+     scribbler
+     owl
+     easy-fix
+     multilingual
+     mover
+     world-balance
+     narcissist
+     blamer
+     lucky
+     mark-of-the-beast
+     commenter
+     ocd
+     holy-war
+     fat-ass
+     deal-with-it
+     dangerous-game
+     empty-commit
+     time-get
+     for-stallman
+     change-of-mind
+     wrecking-ball
+     alzheimers
+     nothing-to-hide
      ]))
 
 (def all-timeline-scanners
   [catchphrase
    anniversary
+   flash
+   waste
+   loneliness
+   necromancer
+   commit-get
+   collision
+   hydra
+   peacemaker
+   combo
+   combo-breaker
+   worker-bee
+   oops
+   what-happened-here
+   all-things-die
+   goodboy
    ])
 
+(def all-meta-achievements
+  [gandalf
+   unpretending
+   munchkin])
